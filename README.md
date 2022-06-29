9d4fff8947bd8887361a779246db777d753b91f6
watter08 <watter08@hotmail.es>
2022-06-29 15:38:24 -0400

    adding functions and repository

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # wdeaza-functions-utility
  npm module that provides functions for possible form validations
+
+# List Functions
+## isBooleano
+## GetUuId
+## HasErrorShowError
+## hasEnoughLength
+## isEmptyArray
+## isEmptyObject
+## isInvalidInput
+## isInvalidInputChild
+## isMatchString
+## isNumberValid
+## isValidDate
+## isValidEmail
+## isValidPassword
+## isValidPhone
+
---
+++ src/index.js
@@ -0,0 +1,148 @@
+const uuid = require('uuid');
+
+
+/*************************************************************
+ * GET UNIQUE ID OF KEY
+ * **********************************************************/
+
+ const GetUuId = () => { return String(uuid.v4()) };
+
+
+/*************************************************************
+ * HAS ERROR AND SHOW ERROR
+ * **********************************************************/
+
+  const HasErrorShowError = (errors) => { 
+   for(var err in errors) {
+     console.log(errors[err])
+   }
+   return Object.entries(errors).length > 0;
+  };
+
+
+
+
+/*************************************************************
+ * VALIDAR SI EL INPUT TIENE ERRORES DE VALIDACION
+ * **********************************************************/
+
+     const isInvalidInput = (errors, name)   => {  return errors && errors[name] ? true : false };
+
+     const isInvalidInputChild = (errors, name)   => {  
+        let bole = Object.entries(errors).length <= 0 ? false :  errors && errors?.Formulario[name.split('.')[1]] ? true : false ;
+        return bole;
+    
+    };
+
+
+/*************************************************************
+ * IF VALID PHONE
+ * **********************************************************/
+  
+   function isValidPhone(string = "")   {
+    const regEx = /^(\()?\d{3}-?(\))?\d{3}-?\d{4}$/;
+    return (string && string.match(regEx)) ? true : false;
+  }
+
+  
+ /*************************************************************
+ * IS HAS ENOUGTH CHARACTER
+ * **********************************************************/
+
+   const hasEnoughLength = (string, minimunCount)   => {
+    const regEx = /[a-zA-Z\t\h]+|(^$)/;
+    return (string?.length >= minimunCount && string.match(regEx) && string?.trim()) ? true : false;
+  };
+
+
+
+ /*************************************************************
+ * IS VALID NUMBER
+ * **********************************************************/
+
+   const isNumberValid = (number)   => {
+    return typeof number === 'number' && isFinite(number);
+  }
+
+
+
+ /*************************************************************
+ * IS VALID EMAIL
+ * **********************************************************/
+
+   const isValidEmail = (string)   => {
+    const regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    return (string && string.match(regEx)) ?true : false;
+  }
+
+
+/*************************************************************
+ * IS EMPTY ARRAY
+ * **********************************************************/
+
+   const isEmptyArray = (array)  => {
+    return (array && array.length === 0) ? true : false;
+  }
+  
+
+/*************************************************************
+ * IS EMP TY OBJECT
+ * **********************************************************/
+
+   const isEmptyObject = (obj)   => {
+    return Object.keys(obj).length === 0;
+  }
+
+
+ /*************************************************************
+ * IS VALID DATE
+ * **********************************************************/
+   const isValidDate = (date)  =>  {
+    return date instanceof Date  && Object.prototype.toString.call(date) === '[object Date]';
+  }
+  
+
+
+ /*************************************************************
+ * IS VALID PASSWORD
+ * **********************************************************/
+
+   const isValidPassword = (string)  => {
+    const regEx = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+    return (string && string.match(regEx)) ? true : false;
+  }
+
+
+ /*************************************************************
+ * IS BOOLEAN
+ * **********************************************************/
+
+   const isBooleano = (bool)   => {
+    return 'boolean' === typeof bool;
+  }
+
+
+ /*************************************************************
+ * IS MATCH STRING
+ * **********************************************************/
+
+   const isMatchString = (NewPassword , OldPassword)   => {
+    return NewPassword?.toString()?.toLowerCase() === OldPassword?.toString()?.toLowerCase() ? true : false;
+}
+
+module.exports = {
+    isMatchString,
+    isBooleano,
+    isValidPassword,
+    isValidDate,
+    isEmptyObject,
+    isEmptyArray,
+    isValidEmail,
+    isNumberValid,
+    hasEnoughLength,
+    isValidPhone,
+    isInvalidInputChild,
+    isInvalidInput,
+    HasErrorShowError,
+    GetUuId,
+}
